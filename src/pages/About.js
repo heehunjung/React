43e563ee5202b95ade50abb840163350dcfc0db0
heehunjung <slrkqk2@naver.com>
2024-05-22 01:33:52 +0900

    init

---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+	return <h1>About 화면</h1>;
+} // javascript 함수를 사용
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+	return <h1>Home 화면</h1>;
+} // javascript 함수를 사용
+
+export default Home;
